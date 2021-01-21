3378b2aa17f1f87bd8b36c82a1a6c853c15a38db
Jordan Rilassi <jordan.rilassi@gmail.com>
2021-01-21 17:08:34 +0100

    SwiftUI tuto part 2

---
+++ NanoedEvents/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  NanoedEvents
+//
+//  Created by Jordan Rilassi on 21/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
