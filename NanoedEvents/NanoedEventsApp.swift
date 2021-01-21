df9de6316ad529d507c761d324037f184842f811
Jordan Rilassi <jordan.rilassi@gmail.com>
2021-01-21 15:33:18 +0100

    SwiftUI tuto part1

---
+++ NanoedEvents/NanoedEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanoedEventsApp.swift
+//  NanoedEvents
+//
+//  Created by Jordan Rilassi on 21/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct NanoedEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
